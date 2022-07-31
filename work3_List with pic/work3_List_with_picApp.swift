558d777a094cad375b2f828d0b34ea02b20224bc
Ashin <ashinwangboy@gmail.com>
2022-07-31 20:40:28 +0800

    Initial Commit

---
+++ work3_List with pic/work3_List_with_picApp.swift
@@ -0,0 +1,17 @@
+//
+//  work3_List_with_picApp.swift
+//  work3_List with pic
+//
+//  Created by Ashin Wang on 2022/7/31.
+//
+
+import SwiftUI
+
+@main
+struct work3_List_with_picApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ work3_List with picUITests/work3_List_with_picUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  work3_List_with_picUITestsLaunchTests.swift
+//  work3_List with picUITests
+//
+//  Created by Ashin Wang on 2022/7/31.
+//
+
+import XCTest
+
+class work3_List_with_picUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
